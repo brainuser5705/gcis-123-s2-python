da70593118a9dca0e2c9f006c668d6b9911371e0
Jin Moon <67706639+jym2584@users.noreply.github.com>
2021-09-18 00:38:20 -0400

    example problems for sunday's session

---
+++ week5/b/loops.py
@@ -0,0 +1,58 @@
+
+def print_even():
+    """
+    1a. Create a function that prints every even numbers, up to 100
+
+    1b. Now edit the function so that it takes in a parameter and returns a sum of all numbers in the loop 
+    """
+    pass
+
+
+string = "hello this is a string!"
+
+def print_vowels(string):
+    """
+    2. Create a function that prints the vowels of a string and returns the number of vowels 
+
+    Example input:
+    string = "hello world this is a vowel and stuff, hopefully will contain!!!! some vowels"
+    returns: 16
+    """
+    pass
+
+
+def fizzbuzz(number):
+    """
+    3. Use test driven development to create a function that implements fizzbuzz
+
+    Fizzbuzz:
+    - prints FizzBuzz if the modulo of 3 and 5 from a number has a remainder of 0
+    - prints Fizz if only the modulo of 3 has a remainder of 0
+    - prints Buzz if only the module of 5 has a remainder of 0
+    """
+    pass
+
+
+
+def longest_line():
+    """
+    4. Create a function that accepts an infinite number of input that prints and keeps track
+    of the longest line that was entered (the most characters in a line) until the user
+    presses ENTER to quit the program
+
+    Example input:
+    >> "Hello"
+    Longest line: Hello
+
+    >> "Hey this is Patrick"
+    Longest line: Hey this is Patrick
+
+    >> "Hi"
+    Longset line: Hi
+
+    >> "supercalifragalisticexpialadocious"
+    Longest line: supercalifragalisticexpialadocious
+
+    >> 
+    """
+    pass
